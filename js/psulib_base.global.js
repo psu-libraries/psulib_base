73996a84e64a1fcdaf6d2f69ece2fd7067ca0e38
Andrew Gearhart <amg32@psu.edu>
2024-02-13 11:10:50 -0500

    init setup of the psulib_base theme

---
+++ js/psulib_base.global.js
@@ -0,0 +1,9 @@
+(function (Drupal, once) {
+  Drupal.behaviors.psulib_base = {
+    attach: function (context, settings) {
+      document.documentElement.style.setProperty(
+        '--scrollbar-width', (window.innerWidth - document.documentElement.offsetWidth + 1) + 'px'
+      );
+    }
+  };
+})(Drupal, once);
