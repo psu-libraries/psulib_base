b29ef2990377290cdd7e08b8646f3e36e954745b
Dann Bohn <djb44@psu.edu>
2024-02-16 13:21:57 -0500

    mix java

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,7 @@
 
 let mix = require('laravel-mix');
 
-// mix.js('src/app.js', 'dist').setPublicPath('dist');
-// mix.js('src/app.js', 'js');
 mix.sass('scss/style.scss', 'css');
+
+mix.combine('js/base', 'js/application.js');
 
