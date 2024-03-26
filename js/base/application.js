da6ea65daacfd52f7e3ef192f4e3486f5df6f548
Dann Bohn <djb44@psu.edu>
2024-03-26 12:03:51 -0400

    empty out base js

--- js/base/application.js
+++
@@ -1,9 +0,0 @@
-(function (Drupal, once) {
-    Drupal.behaviors.psulib_base = {
-      attach: function (context, settings) {
-        document.documentElement.style.setProperty(
-          '--scrollbar-width', (window.innerWidth - document.documentElement.offsetWidth + 1) + 'px'
-        );
-      }
-    };
-  })(Drupal, once);
