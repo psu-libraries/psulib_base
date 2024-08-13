1800b0cf8654287670e8df9d547bf15e163c6136
Mike Henninger <msh6004@psu.edu>
2024-08-13 10:05:49 -0400

    Issue #157 - Updating webpack mix to compile js in component, and compile the new psul-bootstrap.js file.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,20 +11,37 @@ const defaultSassOptions = {
 // Compile global SCSS files.
 mix.sass('scss/style.scss', 'dist/css').options(defaultSassOptions);
 for (const sourcePath of glob.sync("scss/components/**/*.scss")) {
-    mix.sass(sourcePath, 'dist/css').options(defaultSassOptions);;
+  mix.sass(sourcePath, 'dist/css').options(defaultSassOptions);;
 }
 
 // Compile SDC scss files.
 for (const sourcePath of glob.sync("components/**/*.scss")) {
-    const destinationPath = sourcePath.replace(/\.scss$/, ".css");
-    mix.sass(sourcePath, destinationPath).options(defaultSassOptions);;
+  const destinationPath = sourcePath.replace(/\.scss$/, ".css");
+  mix.sass(sourcePath, destinationPath).options(defaultSassOptions);;
 }
 
 // Combine custom javascript into the application.js file.
 mix.combine('js/base', 'dist/js/application.js');
 
+// Build submit of bootstrap styles.
+mix.js('js/psul-bootstrap.js', 'dist/js/psul-bootstrap.js').webpackConfig({
+  // Exclude popperjs library.  This will be loaded separately.
+  externals: {
+    ["@popperjs/core"]:{
+      root: "@popperjs/core"
+    }
+  }
+});
+
+// Add buiding process for component javascript.
+for (const sourcePath of glob.sync("components/**/src/*.js")) {
+  const destinationPath = sourcePath.replace(/\/src\//, "\/");
+  mix.js(sourcePath, destinationPath);
+}
+
 // Copy bootstrap javascript into dist/js directory.
 mix.copyDirectory('node_modules/bootstrap/dist/js/', 'dist/js');
+
 mix.copy('node_modules/@popperjs/core/dist/umd/popper.min.js', 'dist/js');
 mix.copy('node_modules/@popperjs/core/dist/umd/popper.min.js.map', 'dist/js');
 
